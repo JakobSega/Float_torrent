7f228b3be9a1c1a6b56e3acca132614287470e47
Jakob Šega <jakob.sega.13@gmail.com>
2024-08-01 12:49:53 +0200

    Updated main.rs for elves and imposter.

--- projekt-primer/generator_imposter/src/main.rs
+++ projekt-primer/generator_imposter/src/main.rs
@@ -9,7 +9,6 @@ use hyper::Error;
 use hyper::{body::Body, Method, Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
-use sequence::models::Range;
 use serde::{Deserialize, Serialize};
 use sequence::constant::Constant;
 use sequence::models::Sequence;
@@ -17,12 +16,6 @@ use sequence::arithmetic::Arithmetic;
 use sequence::linearcombination::LinearCombination;
 use sequence::linearcombination;
 
-
-use hyper::http::request::Parts;
-use futures::executor::block_on;
-
-use std::string::String;
-use std::vec::Vec;
 
 const NUMBER: u8 = 1; // Change this to 1 or 2 as needed
 
@@ -64,148 +57,9 @@ const fn select_server<'a>(number: u8) -> Server<'a> {
 }
 
 
-static PORT: u16 = MY.port;
 static KEYWORD : &str = MY.keyword;
 static NAME : &str = MY.name;
 const REGISTER : &str =  "http://127.0.0.1:7878/project";
-
-pub mod expression;
-pub mod sequence;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Project {
-    pub name: String,
-    pub ip: String,
-    pub port: u16,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct SequenceSyntax {
-    pub name: String,
-    pub parameters: Vec<f64>,
-    pub sequences: Vec<Box<SequenceSyntax>>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct SequenceRequest {
-    pub range: Range,
-    pub parameters: Vec<f64>,
-    pub sequences: Vec<Box<SequenceSyntax>>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[derive(PartialEq)]
-pub struct SequenceInfo {
-    name: String,
-    description: String,
-    parameters: u32,
-    sequences: u32,
-}
-
-
-
-fn sequences() -> Vec<SequenceInfo> {
-    let mut sequences = Vec::new();
-    sequences.push(SequenceInfo {
-        name: ("Arithmetic".to_owned() + KEYWORD).to_string(),
-        description: "Arithmetic sequence".to_string(),
-        parameters: 2,
-        sequences: 0,
-    });
-    //V primeru serverja 'Elves' hočemo, da ima ime konstanega zaporedja enako kot ime konstantnega na serverju AmongUs, da prečekiramo, da vse deluje ok....
-    let mut k = KEYWORD;
-    if NUMBER == 2 {
-        k = "_Imposter"
-    }
-    let mut m = 0;
-    if NUMBER == 2 {
-        m = 1
-    }
-    sequences.push(SequenceInfo {
-        name: ("Constant".to_owned() + k).to_string(),
-        description: "Constant sequence".to_string(),
-        parameters: 1,
-        sequences: m,
-    });
-    sequences.push(SequenceInfo {
-        name: ("Lin Comb".to_owned() + KEYWORD).to_string(),
-        description: "".to_string(),
-        parameters: 3,
-        sequences: 2,
-    });
-    sequences
-}
-
-fn get_project() -> Project {
-    return Project {
-        name: ("Binarni Banditi".to_owned() + NAME).to_string(),
-        ip: "127.0.0.1".to_string(),
-        port: PORT,
-    };
-}
-
-
-
-
-
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
-    Full::new(chunk.into())
-        .map_err(|never| match never {})
-        .boxed()
-}
-async fn collect_body(req: Request<Incoming>) -> Result<String, hyper::Error> {
-    let max = req.body().size_hint().upper().unwrap_or(u64::MAX);
-    if max > 1024 * 64 {
-        panic!("Body too big");
-    }
-
-    let whole_body = req.collect().await?.to_bytes();
-    let whole_body = std::str::from_utf8(&whole_body).unwrap().to_string();
-    return Ok(whole_body);
-}
-
-
-
-
-fn empty() -> BoxBody<Bytes, hyper::Error> {
-    Empty::<Bytes>::new()
-        .map_err(|never| match never {})
-        .boxed()
-}
-
-async fn send_post(url: String, body: String) -> Result<String, reqwest::Error> {
-    let client = reqwest::Client::new();
-    let res = client.post(url).body(body).send().await?.text().await?;
-    return Ok(res);
-}
-
-fn parse_string_to_vec(input: &str) -> Vec<Option<f64>> {
-    // Remove the brackets and trim whitespace
-    
-    let trimmed_input = input.trim().trim_start_matches('[').trim_end_matches(']').trim();
-    
-    // Split the string by commas and trim whitespace from each part
-    let parts: Vec<&str> = trimmed_input.split(',').map(|s| s.trim().trim_matches('"')).collect();
-    
-    // Convert parts to Vec<Option<f64>>
-    let result: Vec<Option<f64>> = parts.iter().map(|&s| {
-        if s == "None" {
-            None
-        } else {
-            s.parse::<f64>().ok()
-        }
-    }).collect();
-    
-    result
-}
-
-
-
-async fn send_get(url: String) -> Result<String, reqwest::Error> {
-    let client = reqwest::Client::new();
-    let res = client.get(url).send().await?.text().await?;
-    return Ok(res);
-}
 
 
 #[tokio::main]
